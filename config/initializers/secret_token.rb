151e144a2c53a4e259463d7097108b8a0ef67265
Dan Calacci <dan.calacci@gmail.com>
2012-10-18 01:51:23 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beefsteak::Application.config.secret_token = 'd65b8d6f96a9f37d36a5162c43c2bb5dee076f2f0bc2fc248deb79f1e210c97f44728e49055be70e978afcc406b6e6e61b7e022af5bf93147c5b5f8be37d2901'
